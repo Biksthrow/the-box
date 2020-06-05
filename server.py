fe1d4933bec499afb337d5d9b149f0e0d2a364cc
mickael.chau <mickael.chau@epita.fr>
2020-06-06 00:05:32 +0200

    second commit

---
+++ server.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    logo = url_for('static', filename='Logo.svg')
+    image_file = url_for('static', filename='imgHead.png')
+    un_file = url_for('static', filename='un.svg')
+    deux = url_for('static', filename='deux.svg')
+    trois = url_for('static', filename='trois.svg')
+    quatre = url_for('static', filename='quatre.svg')
+    cartel_file  = url_for('static', filename='logoCartel.png')
+    casse_file = url_for('static', filename='casse.png')
+    illusion_file = url_for('static', filename='illusion.png')
+    return render_template('index.html',cartel =cartel_file,illusion =illusion_file,casse =casse_file, img1 =image_file,log =logo, un=un_file, deux =deux,trois=trois,quatre=quatre)
+
+
+
+@app.route('/reservation')
+def reservation():
+    logo = url_for('static', filename='Logo.svg')
+    return render_template('reservation.html',log =logo)
+
+@app.route('/entreprises')
+def entreprises():
+    return render_template('entreprises.html',log =logo)
+
+@app.route('/cadeaux')
+def cadeaux():
+    logo = url_for('static', filename='Logo.svg')
+    return render_template('cadeaux.html',log =logo)
+
+@app.route('/jeux')
+def jeux():
+    logo = url_for('static', filename='Logo.svg')
+    return render_template('jeux.html',log =logo)
+
+@app.route('/faq')
+def faq():
+    logo = url_for('static', filename='Logo.svg')
+    return render_template('faq.html',log =logo)
+
+@app.route('/contact')
+def contact():
+    logo = url_for('static', filename='Logo.svg')
+    return render_template('contact.html',log =logo)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
